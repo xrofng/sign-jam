7f6fe6ff83e5c3b7ac389519c8e3ef3ce4576a7f
XROFNG-VICTUS\user <xrofng@gmail.com>
2025-10-27 18:29:42 +0700

    feat: start implement District ruleset

---
+++ Assets/Scripts/District/HouseRulesetSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "R_", menuName = "SignGame/Ruleset")]
+public class HouseRulesetSO : ScriptableObject
+{
+    public string DistrictName = "Babbling Gum";
+
+
+}
