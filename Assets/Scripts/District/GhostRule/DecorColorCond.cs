acbeeb3fdebb28f24dbdfcc1ff7e68fa14dd7d6a
NinNinJA <ninja.tananat@gmail.com>
2025-10-30 17:05:50 +0700

    Initiate Rule Set Ravenwood

---
+++ Assets/Scripts/District/GhostRule/DecorColorCond.cs
@@ -0,0 +1,31 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class DecorColorCond : HouseCondition
+{
+    [Required]
+    public DecorationSO Decoration;
+    public EComparisonMethod ComparisonMethod = EComparisonMethod.MoreEqual;
+    public int TargetNumber = 1;
+
+
+    protected override string CalculateListLabel()
+    {
+        if (Decoration)
+        {
+            Summary = Decoration.DecorationID + " " + GetComparisonMethodSymbol(ComparisonMethod) + " " + TargetNumber;
+            return Summary;
+        }
+        return " NEED Decoration";
+    }
+
+    protected override bool EvaluateCondition(HouseSO houseSO, House house)
+    {
+        return true;
+    }
+
+    public override HouseGenerationRequest GetGenerationRequest()
+    {
+        return new DecorationGenRequest(Decoration, TargetNumber);
+    }
+}
