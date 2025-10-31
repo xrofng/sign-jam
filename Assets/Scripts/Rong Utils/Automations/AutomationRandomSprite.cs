2c2f17fb367a31d80785e7c0437ddd0dd155a1a1
XROFNG-VICTUS\user <xrofng@gmail.com>
2025-10-31 07:54:45 +0700

    feat: add AutomationRandomSprite

---
+++ Assets/Scripts/Rong Utils/Automations/AutomationRandomSprite.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AutomationRandomSprite : Automation
+{
+    [Header("Obj Ref")]
+    [SerializeField] Sprite[] PossibleSprites;
+    [SerializeField] SpriteRenderer mainSpriteRenderer;
+
+    public SpriteRenderer MainSpriteRenderer
+    {
+        get
+        {
+            if (mainSpriteRenderer == null)
+            {
+                TryGetComponent(out mainSpriteRenderer);
+            }
+            return mainSpriteRenderer;
+        }
+    }
+
+    protected override void DoAutomation()
+    {
+        base.DoAutomation();
+        MainSpriteRenderer.sprite = PossibleSprites[Random.Range(0, PossibleSprites.Length)];
+    }
+}
